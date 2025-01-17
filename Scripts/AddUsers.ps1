50ffdd3cefb75626b06c1eb1a86aee8a5cfe9d85
m0 <104991836+Chungmire@users.noreply.github.com>
2025-01-17 17:09:51 -0500

    Update AddUsers.ps1

--- Scripts/AddUsers.ps1
+++ Scripts/AddUsers.ps1
@@ -1,11 +1,10 @@
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`""
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
-    }
+    $CommandLine = "-NoProfile -ExecutionPolicy Bypass -Command & ([scriptblock]::Create((irm 'https://raw.githubusercontent.com/Chungmire/2025ActiveDirectoryLab/main/Scripts/AddUsers.ps1')))"
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
 }
+
 
 # Check if Active Directory module is available
 if (-not (Get-Module -ListAvailable -Name ActiveDirectory)) {
