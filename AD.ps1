30a42e047326efe587ac979340a2783a9ef2fce9
m0 <104991836+Chungmire@users.noreply.github.com>
2025-01-18 16:04:44 -0500

    Delete AD.ps1

--- AD.ps1
+++
@@ -1,5 +0,0 @@
-# Launch AddUsers script as administrator
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command & ([scriptblock]::Create((irm 'https://raw.githubusercontent.com/Chungmire/2025ActiveDirectoryLab/main/Scripts/AddUsers.ps1')))"
-    Exit
-}
